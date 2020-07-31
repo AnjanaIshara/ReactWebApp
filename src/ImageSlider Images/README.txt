4d56081f4ab76169e95f55f6e4e907da810a4c0c
AnjanaIshara <anjanaishara08@gmail.com>
2020-07-31 23:24:47 +0530

    captions inserted

--- src/ImageSlider Images/README.txt
+++ src/ImageSlider Images/README.txt
@@ -2,4 +2,5 @@ All the images Should be named as
 	>image1
 	>image2
 	>image3
+image width must be 1080px
 all image files should be in jpg file type
--- src/components/Imageslider/Imageslider.js
+++ src/components/Imageslider/Imageslider.js
@@ -1,28 +1,55 @@
 import React, { Component } from 'react';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
-import img1 from '../../ImageSlider Images/image1.jpg';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Carousel } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import img1 from '../../ImageSlider Images/image1.jpg';//Import the images from the ImageSlider Images which is in src->components
 import img2 from '../../ImageSlider Images/image2.jpg';
 import img3 from '../../ImageSlider Images/image3.jpg';
-class Imageslider extends Component{
-    render(){
-        return(
+
+class Imageslider extends Component {
+    render() {
+        return (
+
             <Carousel>
-                
-                <div>
-                    <img src={img1}  alt="Image1"/>
-                    
-                </div>
-                <div>
-                    <img src={img2} alt="Image2" />
-                    
-                </div>
-                <div>
-                    <img src={img3} alt="Image3" />
-                    
-                </div>
+                <Carousel.Item>
+                    <img class="d-block img-fluid mx-auto" src={img1} alt="Firstimage" ></img>
+
+                    <Carousel.Caption>
+                        <h1>Lorem ipsum dolor sit amet</h1>
+                        Capitalize on low hanging fruit to identify a ball park value added
+                        activity set. Override the digital divide with additional clickthroughs Capitalize on low hanging fruit
+                         to identify a ballpark value added activity test<br></br>
+                        <Button variant="primary">GET STARTED</Button>{'   '}
+                        <Button variant="outline-primary">LEARN MORE</Button>
+                    </Carousel.Caption>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <img class="d-block img-fluid mx-auto" src={img2} alt="Secondimage"></img>
+                    <Carousel.Caption>
+                        <h1>Lorem ipsum dolor sit amet</h1>
+                        Capitalize on low hanging fruit to identify a ball park value added
+                        activity set. Override the digital divide with additional clickthroughs Capitalize on low hanging fruit
+                         to identify a ballpark value added activity test<br></br>
+                        <Button variant="primary" >GET STARTED</Button>{'   '}
+                        <Button variant="outline-primary" >LEARN MORE</Button>
+
+
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img class="d-block img-fluid mx-auto" src={img3} alt="Thirdimage"></img>
+                    <Carousel.Caption>
+                        <h1>Lorem ipsum dolor sit amet</h1>
+                        Capitalize on low hanging fruit to identify a ball park value added
+                        activity set. Override the digital divide with additional clickthroughs Capitalize on low hanging fruit
+                         to identify a ballpark value added activity test<br></br>
+                         <Button variant="primary">GET STARTED</Button>{'   '}
+                         <Button variant="outline-primary">LEARN MORE</Button>
+                    </Carousel.Caption>
+                </Carousel.Item>
+
             </Carousel>
-                
         );
     }
 }
